7e673595407285c08687199eba042c59c779cdb6
Damien Rochford <djrochford@gmail.com>
2018-03-03 17:46:54 -0500

    Problems 4, 5 and 6 in Project 1

--- project-1.scm
+++ project-1.scm
@@ -56,10 +56,10 @@
 (define time-to-impact
         (lambda (vertical-velocity elevation)
                 ;output: time-elpased before impact
-                (let ((first-root (root1 -9.8 vertical-velocity elevation)))
+                (let ((first-root (root1 -4.9 vertical-velocity elevation)))
                      (if (> first-root 0)
                          first-root
-                         (root2 -9.8 vertical-velocity elevation)))))
+                         (root2 (/ -9.8 2) vertical-velocity elevation)))))
 
 (time-to-impact 0 0) ; -> 0
 (time-to-impact 0 9.8) ; -> 1
@@ -69,10 +69,10 @@
 (define time-to-height
         (lambda (vertical-velocity elevation target-elevation)
                 ;output: time-elpased before reaching the target-elevation
-                (let ((first-root (root1 -9.8 vertical-velocity (- elevation target-elevation))))
+                (let ((first-root (root1 -4.9 vertical-velocity (- elevation target-elevation))))
                      (if (> first-root 0)
                          first-root
-                         (root2 -9.8 vertical-velocity (- elevation target-elevation))))))
+                         (root2 -4.9 vertical-velocity (- elevation target-elevation))))))
 
 
 (time-to-height 0 0 0) ; -> 0
@@ -92,8 +92,102 @@
 
 ;Problem 4: Flight Distance 
 
-(define degree2radian
-        (lambda (deg)
-                (/ (* deg pi) 180.))) 
+(define pi 3.141592653589793)
+
+(define travel-distance-simple
+        (lambda (elevation velocity angle)
+                (let* ((vy (* (sin angle) velocity))
+                       (vx (* (cos angle) velocity))
+                       (t (time-to-impact vy elevation)))
+                      (* vx t))))
+
+(travel-distance-simple 1 45 0) ; approx 20
+(travel-distance-simple 1 45 (/ pi 4)) ; -> approx 207
+(travel-distance-simple 1 45 (/ pi 2)) ; -> 0, more or less
+
+;Problem 5: What’s the best angle to hit? 
+
+(define (find-biggest-in-stream stream transform iteration limit biggest)
+        (cond ((> iteration limit) biggest)
+              ((> (transform (stream-car stream)) (transform biggest)) 
+               (find-biggest-in-stream (stream-cdr stream) 
+                                       transform
+                                       (+ iteration 1) 
+                                       limit 
+                                       (stream-car stream)))
+              (else (find-biggest-in-stream (stream-cdr stream)
+                                            transform
+                                            (+ iteration 1)
+                                            limit
+                                            biggest))))
+
+(define find-best-angle
+        (lambda (velocity elevation)
+                (define angles (cons-stream 0 (stream-map (lambda (angle) (+ 0.1 angle))
+                                                         angles)))
+                (define (angle-to-distance angle) 
+                        (travel-distance-simple elevation 
+                                                velocity 
+                                                angle))
+        (find-biggest-in-stream angles 
+                                angle-to-distance
+                                0 
+                                16 
+                                0)))
+(find-best-angle 45 1) ; -> about 45
 
 
+;Problem 6: So why aren’t baseball outfields 600 feet deep? 
+
+(define g 9.8)
+
+(define (system-over-time x_0 y_0 u_0 v_0 beta m)
+        (define dt 0.01)
+        (define (step x-y-u-v)
+                (let* ((x (car x-y-u-v))
+                       (y (cadr x-y-u-v))
+                       (u (caddr x-y-u-v))
+                       (v (cadddr x-y-u-v))
+                       (velocity-term (* (/ -1 m)
+                                         beta
+                                         (sqrt (+ (square u) 
+                                                  (square v)))))
+                       (dx (* u dt))
+                       (dy (* v dt))
+                       (du (* velocity-term u dt))
+                       (dv (- (* velocity-term v dt)
+                              (* g dt))))
+                      (list (+ x dx)
+                            (+ y dy)
+                            (+ u du)
+                            (+ v dv))))
+        (define system (cons-stream (list x_0 y_0 u_0 v_0)
+                                    (stream-map step system)))
+        system)
+
+(define (travel-distance C rho D h V alpha m) 
+        (define A (* pi (/ (square D) 4)))
+        (define beta (* (/ 1 2) C rho A))
+        (define hit-ball (system-over-time 0 h (* V (cos alpha)) (* V (sin alpha)) beta m))
+        (define (find-x-when-y-is-0 stream)
+                (let ((x-y-u-v (stream-car stream)))
+                     (if (> (cadr x-y-u-v) 0)
+                         (find-x-when-y-is-0 (stream-cdr stream))
+                         (car x-y-u-v))))
+        (find-x-when-y-is-0 hit-ball))
+
+;changing speed
+(travel-distance 0.5 1.25 0.074 1 45 (/ pi 4) 0.15) ; -> about 94m
+(travel-distance 0.5 1.25 0.074 1 40 (/ pi 4) 0.15) ; -> about 83m
+(travel-distance 0.5 1.25 0.074 1 35 (/ pi 4) 0.15) ; -> about 71m
+
+;changing angle
+(travel-distance 0.5 1.25 0.074 1 45 (/ pi 4) 0.15) ; -> about 94m -- over the fence
+(travel-distance 0.5 1.25 0.074 1 45 (/ pi 5) 0.15) ; -> about 95m -- over the fence (hmmm...)
+(travel-distance 0.5 1.25 0.074 1 45 (/ pi 3) 0.15) ; -> about 77m
+(travel-distance 0.5 1.25 0.074 1 45 (/ pi 6) 0.15) ; -> about 92m
+(travel-distance 0.5 1.25 0.074 1 45 0 0.15) ; -> about 19m
+
+;in Denver
+(travel-distance 0.5 1.06 0.074 1 45 (/ pi 4) 0.15) ; -> about 101m
+(travel-distance 0.5 1.06 0.074 1 40 (/ pi 4) 0.15) ; -> about 89m
