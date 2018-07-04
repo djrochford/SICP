44c652e34bafa1f473e5a64f6562d6beb73df104
Damien Rochford <djrochford@gmail.com>
2018-07-03 21:25:25 -0400

    Add question for exercise 18

--- Chapter 1/exercise_17+18.scm
+++ Chapter 1/exercise_17+18.scm
@@ -21,6 +21,8 @@
 
 (fast-multiply 90 47) ; 4230
 
+;Exercise 1.18.  Using the results of exercises 1.16 and 1.17, devise a procedure that generates an iterative process for multiplying two integers in terms of adding, doubling, and halving and uses a logarithmic number of steps.
+
 (define (fast-multiply-iter a b)
         (define (iter sum a b)
                 (cond ((= b 0) sum)
