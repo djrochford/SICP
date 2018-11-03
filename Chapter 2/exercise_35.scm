6dea681c6f9fb16942f9534b1ea08b5031dce3dd
Damien Rochford <djrochford@gmail.com>
2018-11-03 13:04:09 -0500

    Add question to exercise 2.35

--- Chapter 2/exercise_35.scm
+++ Chapter 2/exercise_35.scm
@@ -1,8 +1,13 @@
+;Exercise 2.35.  Redefine `count-leaves` from section 2.2.2 as an accumulation/
+
+"The accumulation procedure:"
 (define (accumulate op initial sequence)
         (if (null? sequence)
         initial
         (op (car sequence)
             (accumulate op initial (cdr sequence)))))
+
+"The answer:"
 
 (define (count-leaves t)
         (accumulate + 0 (map (lambda (tree)
@@ -11,4 +16,4 @@
                                          (count-leaves tree))) 
                              t)))
 
-(count-leaves (list 1 2 (list 3 (list 4 5) (list 6 7) 8 9) 10 11 (list 12)))
+(count-leaves (list 1 2 (list 3 (list 4 5) (list 6 7) 8 9) 10 11 (list 12))) ;12
