82b632ac3c99c8f1b29bd6b8dd4760f2ad0810b5
Damien Rochford <djrochford@gmail.com>
2018-11-18 10:22:01 -0600

    Add quesitno to exercise 2.39

--- Chapter 2/exercise_39.scm
+++ Chapter 2/exercise_39.scm
@@ -1,15 +1,5 @@
-(define (fold-right op initial sequence)
-        (if (null? sequence)
-        initial
-        (op (car sequence)
-            (fold-right op initial (cdr sequence)))))
-
-(define (fold-left op initial sequence)
-        (define (iter result rest)
-                (if (null? rest)
-                    result
-                    (iter (op result (car rest)) (cdr rest))))
-        (iter initial sequence))
+;Exercise 2.39.   Complete the following definitions of reverse (exercise 2.18)
+;in terms of fold-right and fold-left from exercise 2.38.
 
 (define (reverse-right sequence)
         (fold-right (lambda (x y)
@@ -17,10 +7,10 @@
                     () 
                     sequence))
 
-(reverse-right (list 1 2 3 4 5))
+(reverse-right (list 1 2 3 4 5)) ;(5 4 3 2 1)
 
 
 (define (reverse-left sequence)
         (fold-left (lambda (x y) (cons y x)) () sequence))
 
-(reverse-left (list 1 2 3 4 5))
+(reverse-left (list 1 2 3 4 5)) ;(5 4 3 2 1)
