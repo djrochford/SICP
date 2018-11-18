9fc6bdfb42acaa3c0e25b5e5a5369a7207b59818
Damien Rochford <djrochford@gmail.com>
2018-11-17 20:47:13 -0600

    Clean up exercise 2.37

--- Chapter 2/exercise_37.scm
+++ Chapter 2/exercise_37.scm
@@ -1,3 +1,35 @@
+;Exercise 3.37 Suppose we represent vectors v = (v_i) as sequences of numbers, 
+;and matrices m = (m_(ij)) as sequences of vectors (the rows of the matrix). For example, the matrix
+; _     _
+;|1 2 3 4|
+;|4 5 6 6|
+;|6 7 8 9|
+;-       -
+;is represented as the sequence 
+
+;((1 2 3 4) (4 5 6 6) (6 7 8 9))
+
+;With this representation, 
+;we can use sequence operations to concisely express the basic matrix and vector operations.
+;These operations (which are described in any book on matrix algebra) are the following:
+
+;(dot-product v w) returns Sum_i (v_i * w_i)
+;(matrix-*-vector m v) returns the vector t, where t_i = Sum_j (m_(ij) * v_j)
+;(matrix-*-matrix m n) returns the matrix p, where p_(ij) = Sum_k (m_(ik) n_(kj))
+;(transpose m) returns the matrix n, where n_(ij) = m_(ji)
+
+;We can define the dot product as:
+
+(define (dot-product v w)
+        (accumulate + 0 (map * v w)))
+
+;Fill in the missing expressions in the following procedures 
+;for computing the other matrix operations. 
+;(The procedure accumulate-n is defined in exercise 2.36.)
+
+"Just so we have it handy:"
+
+
 (define (accumulate op initial sequence)
         (if (null? sequence)
         initial
@@ -10,9 +42,7 @@
             (cons (accumulate op init (map (lambda (seq) (car seq)) seqs))
                   (accumulate-n op init (map (lambda (seq) (cdr seq)) seqs)))))
 
-(define (dot-product v w)
-        (accumulate + 0 (map * v w)))
-
+"The answers:"
 (define (matrix-*-vector m v)
         (map (lambda (w) (dot-product w v)) m))
 
@@ -20,15 +50,15 @@
 
 (define V (list 1 1 1 1))
 
-(matrix-*-vector M V)
+(matrix-*-vector M V) ;(10 21 30)
 
 (define (transpose mat)
-        (accumulate-n cons () mat))
+        (accumulate-n cons () mat)) 
 
-(transpose M)
+(transpose M) ;((1 4 6) (2 5 7) (3 6 8) (4 6 9))
 
 (define (matrix-*-matrix m n)
         (let ((cols (transpose n)))
              (map (lambda (v) (matrix-*-vector cols v)) m)))
 
-(matrix-*-matrix M (transpose M))
+(matrix-*-matrix M (transpose M)) ;((30 56 80) (56 113 161) (80 161 230))
