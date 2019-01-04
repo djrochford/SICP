ffdc7109aaf1da8abf9eb24fa2c7619acefe99ca
Damien Rochford <djrochford@gmail.com>
2019-01-03 21:09:43 -0600

    Add question, some notes to exercise 2.59

--- Chapter 2/exercise_59.scm
+++ Chapter 2/exercise_59.scm
@@ -1,12 +1,16 @@
+;Exercise 2.59.  Implement the union-set operation for the unordered-list representation of sets.
+
+"The `element-of-set` function, from the book, will be handy:"
 (define (element-of-set? x set)
         (cond ((null? set) false)
         ((equal? x (car set)) true)
-(else (element-of-set? x (cdr set)))))
+        (else (element-of-set? x (cdr set)))))
 
+"Now here's an answer:"
 (define (union-set set1 set2)
         (cond ((null? set2) set1)
               ((null? set1) set2)
               ((element-of-set? (car set1) set2) (union-set (cdr set1) set2))
               (else (cons (car set1) (union-set (cdr set1) set2)))))
 
-(union-set '(1 2 3 4) '(1 3 5 9))
+(union-set '(1 2 3 4) '(1 3 5 9)) ;(2 4 1 3 5 9)
