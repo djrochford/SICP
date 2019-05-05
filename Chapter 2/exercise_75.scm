13fb8def00604b6ed31351f9079b3ed91c49f847
Damien Rochford <djrochford@gmail.com>
2019-05-05 18:32:11 -0500

    Include question in exercise 2.75

--- Chapter 2/exercise_75.scm
+++ Chapter 2/exercise_75.scm
@@ -1,7 +1,11 @@
+;Exercise 2.75.  Implement the constructor `make-from-mag-ang` in message-passing style.
+;This procedure should be analogous to the `make-from-real-imag` procedure given above
+
 (define (make-from-mag-ang m a)
         (define (dispatch op)
                 (cond ((eq? op 'real-part) (* m (cos a)))
                       ((eq? op 'imag-part) (* m (sin a)))
                       ((eq? op 'magnitude) m)
-                      ((eq? op 'angle') a)))
+                      ((eq? op 'angle') a)
+                      (else "Unknown op -- MAKE-FROM-MAG-ANG", op)))
         dispatch)
