65465379eeea33dd58a84fd7dbcc82a1cfc22863
Damien Rochford <djrochford@gmail.com>
2019-06-16 14:13:57 -0500

    Exercise 2.92

---
+++ Chapter 2/exercise_92.scm
@@ -0,0 +1,12 @@
+;Exercise 2.92.  By imposing an ordering on variables, extend the polynomial package
+;so that addition and multiplication of polynomials works for polynomials in different
+;variables. (This is not easy!)
+
+
+"This really isn't easy. First we need a way to expand polynomials into an alternate
+representation in which no polynomial appears as a coefficient of a term; all
+variables need to be out at the same level. Then we need a way to collect terms. Then
+we need a way to convert that representation back into our standard polynomial representation
+expressing it as a polynomial in a given variable.
+
+I may yet do this, but life is short..."
