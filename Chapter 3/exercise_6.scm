638cf841519304675a5d5f5ad9313ac2a9813a8f
Damien Rochford <djrochford@gmail.com>
2019-06-20 08:00:20 -0500

    Add question to exercise 3.6

--- Chapter 3/exercise_6.scm
+++ Chapter 3/exercise_6.scm
@@ -1,3 +1,11 @@
+;Exercise 3.6.  It is useful to be able to reset a random-number generator to produce
+;a sequence starting from a given value. Design a new `rand` procedure that is called
+;with an argument that is either the symbol `generate` or the symbol `reset` and behaves
+;as follows: `(rand 'generate)` produces a new random number; `((rand 'reset) <new-value>)`
+;resets the internal state variable to the designated <new-value>. Thus, by resetting the
+;state, one can generate repeatable sequences. These are very handy to have when testing
+;and debugging programs that use random numbers.
+
 (define (rand command)
         (define state (random 1.0))
         (cond ((eq? command 'generate) (rand-update state))
