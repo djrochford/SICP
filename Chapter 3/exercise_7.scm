bbb3510ad00c415db32f5a91347d7450bfbc18d5
Damien Rochford <djrochford@gmail.com>
2019-06-20 08:13:22 -0500

    Clean up exercise 3.7

--- Chapter 3/exercise_7.scm
+++ Chapter 3/exercise_7.scm
@@ -1,3 +1,21 @@
+;Exercise 3.7.  Consider the bank account objects created by `make-account`, with the password
+;modification described in exercise 3.3. Suppose that our banking system requires the ability
+;to make joint accounts. Define a procedure `make-joint` that accomplishes this. `Make-joint`
+;should take three arguments. The first is a password-protected account. The second argument
+;must match the password with which the account was defined in order for the `make-joint`
+;operation to proceed. The third argument is a new password. `Make-joint` is to create an
+;additional access to the original account using the new password. For example, if `peter-acc`
+;is a bank account with password `open-sesame`, then
+
+;(define paul-acc
+;  (make-joint peter-acc 'open-sesame 'rosebud))
+
+;will allow one to make transactions on `peter-acc` using the name `paul-acc` and the
+;password `rosebud`. You may wish to modify your solution to exercise 3.3 to accommodate
+;this new feature.
+
+"Here for reference is my 3.3 solution. I will *not* be altering it."
+
 (define (make-account balance password)
         (define (withdraw amount)
                 (if (>= balance amount)
@@ -14,6 +32,8 @@
                       (else (error "Unknown request -- MAKE-ACCOUNT" m))))
         dispatch)
 
+
+"Now `make-joint`"
 (define (make-joint account first-password second-password)
         (if ((account first-password 'withdraw) 0)
             (lambda (pswd m)
