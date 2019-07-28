c6a4f4eecc2f28cbdd0e04f26f50f0bc04e0f3a0
Damien Rochford <damien@admithub.com>
2019-07-28 17:53:38 -0400

    Exercise 3.25, some 3.24

--- Chapter 3/exercise_24.scm
+++ Chapter 3/exercise_24.scm
@@ -1,3 +1,12 @@
+;Exercise 3.24.  In the table implementations above, the keys are tested for equality
+;using `equal?` (called by `assoc`). This is not always the appropriate test. For instance,
+;we might have a table with numeric keys in which we don't need an exact match to the
+;number we're looking up, but only a number within some tolerance of it. Design a table
+;constructor `make-table` that takes as an argument a `same-key?` procedure that will be
+;used to test "equality" of keys. `Make-table` should return a dispatch procedure that can
+;be used to access appropriate `lookup` and `insert!` procedures for a local table.
+
+"Here for reference are the original `assoc` and `make-table`."
 (define (assoc key records)
         (cond ((null? records) false)
               ((equal? key (caar records)) (car records))
@@ -31,25 +40,27 @@
                            (else (error "Unknown operation -- TABLE" m))))
              dispatch))
 
+"Here is my altered `assoc` and the difference `make-table` that uses it."
+
 (define (assoc-equivalent key records same-key?)
         (cond ((null? records) false)
               ((same-key? key (caar records)) (car records))
-              (else (assoc key (cdr records)))))
+              (else (assoc-equivalent key (cdr records)))))
 
 (define (make-equivalence-table)
         (let ((local-table (list '*table*)))
              (define (lookup key-1 key-2 same-key?)
-                     (let ((subtable (assoc key-1 (cdr local-table) same-key?)))
+                     (let ((subtable (assoc-equivalent key-1 (cdr local-table) same-key?)))
                           (if subtable
-                              (let ((record (assoc key-2 (cdr subtable) same-key?)))
+                              (let ((record (assoc-equivalent key-2 (cdr subtable) same-key?)))
                                    (if record
                                        (cdr record)
                                        false))
                               false)))
              (define (insert! key-1 key-2 value same-key?)
-                     (let ((subtable (assoc key-1 (cdr local-table) same-key?)))
+                     (let ((subtable (assoc-equivalent key-1 (cdr local-table) same-key?)))
                           (if subtable
-                              (let ((record (assoc key-2 (cdr subtable) same-key?)))
+                              (let ((record (assoc-equivalent key-2 (cdr subtable) same-key?)))
                                    (if record
                                    (set-cdr! record value)
                                    (set-cdr! subtable 
--- Chapter 3/exercise_25.scm
+++ Chapter 3/exercise_25.scm
@@ -1,27 +1,68 @@
-(define (assoc key records)
-        (cond ((null? records) #f)
-              ((equal? key (caar records)) (car records))
-              (else (assoc key (cdr records)))))
+;Exercise 3.25.  Generalizing one- and two-dimensional tables, show how to implement a table
+;in which values are stored under an arbitrary number of keys and different values may be
+;stored under different numbers of keys. The `lookup` and `insert!` procedures should take as
+;input a list of keys used to access the table.
+
+"For reference, here's the two-key `make-table:"
+(define (make-table)
+        (let ((local-table (list '*table*)))
+             (define (lookup key-1 key-2)
+                     (let ((subtable (assoc key-1 (cdr local-table))))
+                          (if subtable
+                              (let ((record (assoc key-2 (cdr subtable))))
+                                   (if record
+                                       (cdr record)
+                                       false))
+                              false)))
+             (define (insert! key-1 key-2 value)
+                     (let ((subtable (assoc key-1 (cdr local-table))))
+                          (if subtable
+                              (let ((record (assoc key-2 (cdr subtable))))
+                                   (if record
+                                       (set-cdr! record value)
+                                       (set-cdr! subtable 
+                                                 (cons (cons key-2 value) (cdr subtable)))))
+                              (set-cdr! local-table
+                                        (cons (list key-1 (cons key-2 value))
+                                        (cdr local-table)))))
+                     'ok)    
+             (define (dispatch m)
+                     (cond ((eq? m 'lookup-proc) lookup)
+                           ((eq? m 'insert-proc!) insert!)
+                           (else (error "Unknown operation -- TABLE" m))))
+             dispatch))
+
+"Now, my generalised `make-table`:"
+
+(define (is-record? subtable)
+        (not (pair? (cdr subtable))))
 
 (define (make-table)
         (let ((local-table (list '*table*)))
              (define (lookup keys)
                      (let ((subtable (assoc (car keys) (cdr local-table))))
                           (cond ((eq? subtable #f) #f)
-                                ((not (or (pair? (car subtable)) (pair? (cdr subtable))))) (cdr subtable))
-                                (else (lookup (cdr keys) (cdr subtable)))))
-             (define (insert! key-1 key-2 value)
-                     (let ((subtable (assoc key-1 (cdr local-table))))
-                          (if subtable
-                              (let ((record (assoc key-2 (cdr subtable))))
-                                   (if record
-                                   (set-cdr! record value)
-                                   (set-cdr! subtable 
-                                             (cons (cons key-2 value) (cdr subtable)))))
-                              (set-cdr! local-table
-                                        (cons (list key-1 (cons key-2 value))
-                                        (cdr local-table)))))
-                     'ok)    
+                                ((is-record? subtable) (if (null? (cdr keys)) 
+                                                                  (cdr subtable)
+                                                                  #f))
+                                (else (lookup (cdr keys) (cdr subtable))))))
+             (define (insert! keys value)
+                     (define (insert-iter remaining-keys level-up)
+                             (let* ((current-key (car keys))
+                                    (subtable (assoc current-key (cdr level-up))))
+                                    (if subtable
+                                        (if (null? (cdr remaining-keys))
+                                            (set-cdr! subtable value))
+                                            (insert-iter (cdr remaining-keys, subtable))
+                                        (set-cdr! level-up
+                                                  (cons (cons current-key value)
+                                                        (cdr level-up))))))
+                     (if (null? keys)
+                         local-table
+                         (if (null? (cdr keys))
+                             (set-cdr! local-table (cons (car keys value) (cdr local-table))))
+                             (insert-iter keys local-table)))
+
              (define (dispatch m)
                      (cond ((eq? m 'lookup-proc) lookup)
                            ((eq? m 'insert-proc!) insert!)
