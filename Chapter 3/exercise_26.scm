5b5213c17550f9e3879843167fb677bc03313c83
Damien Rochford <damien@admithub.com>
2019-08-07 14:52:18 +0300

    Improve 3.26 answer

--- Chapter 3/exercise_26.scm
+++ Chapter 3/exercise_26.scm
@@ -4,11 +4,13 @@
 ;value) records are organized using a binary tree, assuming that keys can be ordered in some way (e.g.,
 ;numerically or alphabetically). (Compare exercise 2.66 of chapter 2.)
 
-"Here's the idea. Each node of the tree is a key-value pair. The tree is arranged such that, for any
-given node, with key K, all nodes on the left sub-tree have keys k < K, and all nodes on the right
-sub-tree have keys k > K. You need to be careful, when insertng into the tree, that you preserve 
-this property (I won't prove that this is possible for arbitrary sequences of keys here, but it is).
-To find a record, you start at the root node, and compare your key with the node's key. If the keys
-are the same, you return the value. Otherwise, if you're key is lower in the order than the root node's
-key, you recusively do the same thing with the left sub-tree; if, on the other hand, your key is higher
-than the root nodes key, you recursively do the same thing with the right sub-tree."
+"Here's the idea. Each node of the tree is a key-value pair (where we allow a null value to be a degenerate
+case of a key-value pair). The tree is arranged such that, for any given node, with key K, all nodes on the
+left sub-tree have keys k < K, and all nodes on the right sub-tree have keys k > K. You need to be careful,
+when insertng into the tree, that you preserve this property, but this isn't very hard to do. You do it like
+this: go to the root node. If the root node is null, insert your key-value pair right there. If the root
+node has key k1 = to your key k2, then replace the value at the node with your value. If k1 > k2,
+recursively do the same thing with the left sub-tree. If k1 < k2, recursively do the same thing with the
+right sub-tree.
+Findind a record is almost the same as inserting, except that you return a value once you find a node such
+that k1 = k2, or you return some not-found value once you hit a null node."
