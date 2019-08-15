50c38153243abdfcb7c23dccb6e25c943ad67c2c
Damien Rochford <damien@admithub.com>
2019-08-15 17:51:35 +0300

    Exercise 3.35

---
+++ Chapter 3/exercise_35.scm
@@ -0,0 +1,26 @@
+;Exercise 3.35.  Ben Bitdiddle tells Louis that one way to avoid the trouble in exercise 3.34 is to define
+;a squarer as a new primitive constraint. Fill in the missing portions in Ben's outline for a procedure to
+;implement such a constraint:
+
+(define (squarer a b)
+        (define (process-new-value)
+                (if (has-value? b)
+                    (if (< (get-value b) 0)
+                        (error "square less than 0 -- SQUARER" (get-value b))
+                        (set-value! a
+                                   (sqrt (get-value b))
+                                   me))
+                    (if (has-value? a)
+                        (set-value! b
+                                    (* (get-value a) (get-value a))))))
+        (define (process-forget-value)
+                (forget-value! a me)
+                (forget-value! b me)
+                (process-new-value))
+        (define (me request)
+                (cond ((eq? request 'I-have-a-value) (process-new-value))
+                      ((eq? request 'I-lose-my-value) (process-forget-value))
+                      (else (error "Unknown request -- SQUARER" request))))
+        (connect a me)
+        (connect b me)
+        me)
