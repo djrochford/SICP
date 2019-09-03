f52b2e59b038dbb4a665b76e2e3bd8ba2ba5b86c
Damien Rochford <damien@admithub.com>
2019-09-02 21:44:31 -0500

    Clean up 3.50, add 3.51

--- Chapter 3/exercise_50.scm
+++ Chapter 3/exercise_50.scm
@@ -1,32 +1,19 @@
+;Exercise 3.50.  Complete the following definition, which generalizes `stream-map`
+;to allow procedures that take multiple arguments, analogous to `map` in section 2.2.3,
+;footnote 12.
+
 (define (stream-map proc . argstreams)
-  (if (null? (car argstreams))
-      the-empty-stream
-      (cons-stream
-       (apply proc (map stream-car argstreams))
-       (apply stream-map
-              (cons proc (map stream-cdr argstreams))))))
+        (if (<??> (car argstreams))
+            the-empty-stream
+            (<??> (apply proc (map <??> argstreams))
+                  (apply stream-map
+                         (cons proc (map <??> argstreams))))))
 
-(define (stream-enumerate-interval low high)
-  (if (> low high)
-      the-empty-stream
-      (cons-stream
-       low
-       (stream-enumerate-interval (+ low 1) high))))
+"Here is my completion:"
 
-(define (show x)
-  (display-line x)
-  x)
-
-(define (display-stream s)
-  (stream-for-each display-line s))
-
-(define (display-line x)
-  (newline)
-  (display x))
-
-(define x (stream-map show (stream-enumerate-interval 0 40)))
-(stream-ref x 5)
-(stream-ref x 7)
-(stream-ref x 5)
-(stream-ref x 2)
-(stream-ref x 34)
+(define (stream-map proc . argstreams)
+        (if (null? (car argstreams))
+            the-empty-stream
+            (cons-stream (apply proc (map stream-car argstreams))
+                         (apply stream-map
+                                (cons proc (map stream-cdr argstreams))))))
