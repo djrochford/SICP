ffc56f2f553ea61d9de7829e90a6ca0ccf636735
Damien Rochford <damien@admithub.com>
2019-09-28 14:57:36 -0500

    Cleanup exercise 3.64

--- Chapter 3/exercise_64.scm
+++ Chapter 3/exercise_64.scm
@@ -1,20 +1,35 @@
+;Exercise 3.64.  Write a procedure `stream-limit` that takes as arguments a stream and a number
+;(the tolerance). It should examine the stream until it finds two successive elements that differ
+;in absolute value by less than the tolerance, and return the second of the two elements. Using this,
+;we could compute square roots up to a given tolerance by
+
+(define (sqrt x tolerance)
+  (stream-limit (sqrt-stream x) tolerance))
+
+"Here is my procedure:"
+
+(define (stream-limit stream tolerance)
+        (if (< (abs (- (stream-car stream) 
+                       (stream-car (stream-cdr stream)))) 
+               tolerance)
+            (stream-car (stream-cdr stream))
+            (stream-limit (stream-cdr stream) tolerance)))
+
+"Here is a test:"
+
 (define (average a b)
         (/ (+ a b) 2))
 
 (define (sqrt-improve guess x)
-  (average guess (/ x guess)))
+        (average guess (/ x guess)))
 
 (define (sqrt-stream x)
-  (define guesses
-    (cons-stream 1.0
-                 (stream-map (lambda (guess) (sqrt-improve guess x))
-                             guesses)))
-  guesses)
+        (define guesses
+                (cons-stream 1.0
+                            (stream-map (lambda (guess) (sqrt-improve guess x))
+                                        guesses)))
+        guesses)
 
-(define (stream-limit stream tolerance)
-        (if (< (abs (- (stream-car stream) (stream-car (stream-cdr stream)))) tolerance)
-            (stream-car (stream-cdr stream))
-            (stream-limit (stream-cdr stream) tolerance)))
 
 (define (sqrt x tolerance)
         (stream-limit (sqrt-stream x) tolerance))
