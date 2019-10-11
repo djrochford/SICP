eff72bdbc8dfa475a8b1badadeeabfa8da2f88d0
Damien Rochford <djrochford@gmail.com>
2019-10-11 18:23:23 -0500

    Add to exercise 3.81

--- Chapter 3/exercise_81.scm
+++ Chapter 3/exercise_81.scm
@@ -1,8 +1,17 @@
+;Exercise 3.81.  Exercise 3.6 discussed generalizing the random-number generator
+;to allow one to reset the random-number sequence so as to produce repeatable sequences
+;of "random" numbers. Produce a stream formulation of this same generator that operates
+;on an input stream of requests to generate a new random number or to reset the sequence
+;to a specified value and that produces the desired stream of random numbers. Don't use
+;assignment in your solution.
+
+"This, for reference, is the exercise 3.6 solution:"
 (define (rand command)
         (define state (random 1.0))
         (cond ((eq? command 'generate) (rand-update state))
               ((eq? command 'reset) (lambda (new-value) (set! state new-value)))))
 
+"Here is a stream version:"
 (define (stream-rand command-stream)
         (define (make-random-stream seed)
                 (cons-stream seed (make-random-stream (rand-update seed))))
@@ -22,4 +31,7 @@
                            (else (error "bad command -- " command)))))
         (make-return-stream command-stream (make-random-stream 1.0)))
 
+
+"(It assumes commands are either `'generate` or a pair of the form `'reset n`, where `n` is
+the seed to be used for further generations."
   
