236a9e13d37631a65f2278027c8e7c308c9ffa3b
Damien Rochford <djrochford@gmail.com>
2019-10-13 12:03:33 -0500

    Clean up 3.82

--- Chapter 3/exercise_82.scm
+++ Chapter 3/exercise_82.scm
@@ -1,3 +1,10 @@
+;Exercise 3.82.  Redo exercise 3.5 on Monte Carlo integration in terms of streams.
+;The stream version of `estimate-integral` will not have an argument telling how many
+;trials to perform. Instead, it will produce a stream of estimates based on successively
+;more trials.
+
+"Here is my solution to 3.5, for reference"
+
 (define (monte-carlo trials experiment)
         (define (iter trials-remaining trials-passed)
                 (cond ((= trials-remaining 0) (/ trials-passed trials))
@@ -19,29 +26,30 @@
         (<= (sqrt (+(* x x) (* y y))) 1))
 
 
-(* 4 (estimate-integral in-unit-circle 0.0 1.0 0.0 1.0 50000))
+(* 4 (estimate-integral in-unit-circle 0.0 1.0 0.0 1.0 50000)) ; 3917/1250 = 3.1336
 
-;-----------------------
+"And here is a stream version"
 
-(define (monte-carlo-stream experiment-stream passed failed)
+(define (monte-carlo-stream experiment)
         (define (next passed failed)
                 (cons-stream (/ passed (+ passed failed))
-                             (monte-carlo (stream-cdr experiment-stream) passed failed)))
-        (if (stream-car experiment-stream)
-            (next (+ passed 1) failed)
-            (next passed (+ failed 1))))
+                             (if (experiment)
+                                 (next (+ passed 1) failed)
+                                 (next passed (+ failed 1)))))
+        (next 1 1)) ;(`1`s instead of `0`s just to avoid division by 0)
 
-(define (random-in-range-stream low high)
-        (define (make-stream seed)
-                (cons-stream (+ low (* seed (- high low)))
-                             (make-stream rand-update seed)))
-        (make-stream rand-init))
+(define (estimate-integral-stream P x1 x2 y1 y2)
+        (define (in-or-out?)
+                (P (random-in-range x1 x2)
+                   (random-in-range y1 y2)))
+        (monte-carlo-stream in-or-out?))
 
-(define (map-successive-pairs f s)
-        (cons-stream (f (stream-car s) (stream-car (stream-cdr s)))
-                      (map-successive-pairs f (stream-cdr (stream-cdr s)))))
+(define (in-unit-circle x y)
+        (<= (sqrt (+(* x x) (* y y))) 1))
 
-(define experiments
-        (map-successive-pairs in-unit-circle (random-in-range-stream 0.0 1.0)))
+(define (scale-stream stream factor)
+        (stream-map (lambda (x) (* x factor)) stream))
 
-(define pi-stream (* 4 (monte-carlo-stream experiments )))
+(define pi-stream (scale-stream (estimate-integral-stream in-unit-circle 0.0 1.0 0.0 1.0) 4))
+
+(stream-ref pi-stream 50000) ; 78528/25001 = 3.14099436...
