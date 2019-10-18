92582a0001ec7d57d009d6ddd07d0db3628f5037
Damien Rochford <djrochford@gmail.com>
2019-10-17 21:53:41 -0500

    Add question to exercise 4.6.

--- Chapter 4/exercise_6.scm
+++ Chapter 4/exercise_6.scm
@@ -1,3 +1,19 @@
+;Exercise 4.6.  `Let` expressions are derived expressions, because
+
+;(let ((<var1> <exp1>) ... (<varn> <expn>))
+;      <body>)
+
+;is equivalent to
+
+;((lambda (<var1> ... <varn>) <body>)
+;  <exp1>
+;  <expn>)
+
+;Implement a syntactic transformation `let->combination` that reduces
+;evaluating `let` expressions to evaluating combinations of the type
+;shown above, and add the appropriate clause to `eval` to handle `let`
+;expressions.
+
 (define (let? exp) (tagged-list? exp 'let))
 (define (let-bindings exp) (cadr exp))
 (define (let-body exp) (cddr exp))
