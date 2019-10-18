5378ac8da315ee32608b186aa334368592b762bd
Damien Rochford <djrochford@gmail.com>
2019-10-17 21:48:03 -0500

    Add stuff to and fix exercises 4.3-4.5

--- Chapter 4/exercise_3.scm
+++ Chapter 4/exercise_3.scm
@@ -1,3 +1,10 @@
+;Exercise 4.3.  Rewrite `eval so that the dispatch is done in data-directed style.
+;Compare this with the data-directed differentiation procedure of exercise 2.73.
+;(You may use the `car` of a compound expression as the type of the expression, as
+;is appropriate for the syntax implemented in this section.)
+
+"Here is the generic-operation approach to `eval` in the book."
+
 (define (old-eval exp env)
         (cond ((self-evaluating? exp) exp)
               ((variable? exp) (lookup-variable-value exp env))
@@ -13,6 +20,8 @@
               ((application? exp) (apply (eval (operator exp) env)
                                          (list-of-values (operands exp) env)))
               (else (error "Unknown expression type -- EVAL" exp))))
+
+"Here is a data-directed version:"
 
 (define (eval exp env)
         (cond ((self-evaluating? exp) exp)
