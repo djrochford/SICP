672113864ce37f106320f716713372ce6bac2df7
Damien Rochford <djrochford@gmail.com>
2019-10-24 19:19:52 -0500

    Fix up exercise 4.11

--- Chapter 4/exercise_11.scm
+++ Chapter 4/exercise_11.scm
@@ -1,3 +1,7 @@
+;Exercise 4.11.  Instead of representing a frame as a pair of lists, we can represent a
+;frame as a list of bindings, where each binding is a name-value pair. Rewrite the
+;environment operations to use this alternative representation.
+
 (define (add-binding-to-frame! var val frame)
         (cons (cons var val)
               frame))
@@ -26,12 +30,11 @@
 
 (define (define-variable! var val env)
         (let ((frame (first-frame env)))
-             (define (scan vars vals)
-                     (cond ((null? vars) (add-binding-to-frame! var val frame))
-                           ((eq? var (car vars)) (set-car! vals val))
-                           (else (scan (cdr vars) (cdr vals)))))
-             (scan (frame-variables frame)
-                   (frame-values frame))))
+             (define (scan frame)
+                     (cond ((null? frame) (add-binding-to-frame! var val frame))
+                           ((eq? (caar frame) var) (set-cdr! (car frame) val))
+                           (else (scan (cdr frame)))))
+             (scan frame)))
 
 (define (extend-environment frame base-env)
         (cons frame base-env))
@@ -40,6 +43,15 @@
         (define (loop frame accumulator)
                 (if (null? frame)
                     accumulator
-                    (loop (cdr frame) (car (caar frame) accumulator))))
+                    (loop (cdr frame) (cons (caar frame) accumulator))))
         (loop frame '()))
+
+(define (frame-values frame))
+        (define (loop frame accumulator)
+                (if (null? frame)
+                    accumulator
+                    (loop (cdr frame) (cons (cdar frame) accumulator))))
+        (loop frame '()))
+
+
 
