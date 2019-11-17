2df2a0298c4952bd748cf2bc9ef7d5ab1c068e58
Damien Rochford <djrochford@gmail.com>
2019-11-16 22:15:51 -0600

    Complete exercise 4.16

--- Chapter 4/exercise_16.scm
+++ Chapter 4/exercise_16.scm
@@ -1,3 +1,27 @@
+;Exercise 4.16.  In this exercise we implement the method just described for
+;interpreting internal definitions. We assume that the evaluator supports `let`
+;(see exercise 4.6).
+
+;a.  Change `lookup-variable-value` (section 4.1.3) to signal an error if the value
+;it finds is the symbol *unassigned*.
+
+"The original `lookup-variable-value`, for reference:"
+
+(define (lookup-variable-value var env)
+        (define (env-loop env)
+                (define (scan vars vals)
+                        (cond ((null? vars) (env-loop (enclosing-environment env)))
+                              ((eq? var (car vars)) (car vals))
+                              (else (scan (cdr vars) (cdr vals)))))
+                (if (eq? env the-empty-environment)
+                    (error "Unbound variable" var)
+                    (let ((frame (first-frame env)))
+                         (scan (frame-variables frame)
+                               (frame-values frame)))))
+        (env-loop env))
+
+"With the added behaviour for `*unassigned*`:"
+
 (define (lookup-variable-value var env)
         (define (env-loop env)
                 (define (scan vars vals)
@@ -13,21 +37,40 @@
                                (frame-values frame)))))
         (env-loop env))
 
+;b.  Write a procedure `scan-out-defines` that takes a procedure body and returns an
+;equivalent one that has no internal definitions, by making the transformation described
+;above.
+
+"This implementation is somewhat inefficient but it's clean."
 (define (scan-out-defines proc-body)
-        (define (add-definition-to-target definition target)
-                (add-to-tail (list (name definition) '*unassigned*')
-                             (car target))
-                (add-to-tail (make-set (name definition) (body definition))))
-        (define (transform-step block target)
-                (if (definition? block)
-                    (add-definition-to-target block (car target))
-                    (add-to-tail block (cdr target))))
-        (define (transform body accumulator)
-                (let (((first-block) (car body)))
-                     (if (null? first-block) 
-                         accumulator
-                         (transform (cdr body) 
-                                    (transform-step first-block
-                                                    accumulator)))))
-        (make-let (transform proc-body (cons '() '()'))))
+        (let* ((definitions (filter definition? proc-body)))
+              (if (null? definitions)
+                  proc-body
+                  (let* ((rest-of-body (filter (lambda (clause) (not (definition? clause)))))
+                         (def-variables (map definition-variable definitions))
+                         (def-values (map definitions-value definitions))
+                         (variable-assignments (map (lambda (var) (cons var '*unassigned*)) def-variables))
+                         (set-statements (map (lambda (var-val-pair) (make-assignment (car var-val-pair) (cdr var-val-pair))) 
+                                              (zip def-variables def-values)))
+                         (let-body (append set-statements rest-of-body)))
+                        (make-let variable-assignments let-body)))))
+"Where 
+ - `(zip list1 list2)` returns a list of the pair of the first elements of `list1` and `list2`, followed by
+the pair of the second elements of `list1` and `list2`, and so on. (Definition is an exercise for the reader.)
+ - `make-let` makes a let-expression in the object-language, and
+ - `make-assignment` makes an assignment expression in the object-language.
+"
+
+;c.  Install scan-out-defines in the interpreter, either in `make-procedure` or in
+;`procedure-body` (see section 4.1.3). 
+
+(define (make-procedure parameters body env)
+        (list 'procedure parameters (scan-out-defines body) env))
+
+;Which place is better? Why?
+"I prefer `make-procedure` because it means that `scan-out-defines` will only be run once per procedure,
+when the procedure is defined, wheras, if `scan-out-defines` were installed in `procedure-body`, it would
+run every time the body of a procedure were accessed."
+
+
                          
