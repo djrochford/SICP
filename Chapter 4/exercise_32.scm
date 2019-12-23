b53c9d31c2d6bfa077ce447a556deac10bbf40ac
Damien Rochford <djrochford@gmail.com>
2019-12-22 21:52:30 -0600

    Exercise 4.32

--- Chapter 4/exercise_32.scm
+++ Chapter 4/exercise_32.scm
@@ -4,7 +4,27 @@
 
 "A relatively obvious way to take advantage of this is to make an infinite tree:"
 
-(define (next-level val)
-        (cons (/ val 2) (+ val (/ val 2))))
+(define (make-tree left-tree node right-tree)
+        (list left-tree node right-tree))
 
-(define cantor (cons (/ 1 2) (map next-level cantor)))
+(define one-tree (make-tree one-tree 1 one-tree))
+
+
+"Fancier:"
+
+(define (left tree) (car tree))
+(define (node tree) (cadr tree))
+(define (right tree) (caddr tree))
+
+(define (tree-map proc tree)
+        (make-tree (tree-map proc (left tree))
+                   (proc (node tree))
+                   (tree-map proc (right tree))))
+
+
+(define power-of-two-fractions (make-tree (tree-map (lambda (x) (/ x 2))
+                                                    power-of-two-fractions)
+                                          (/ 1 2)
+                                          (tree-map (lambda (x) (/ (* 3 x) 2))
+                                                    power-of-two-fractions)))
+
