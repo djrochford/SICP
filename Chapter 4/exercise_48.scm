449ce37fb56e7e968c52d01f4de46f4b7ed47e6f
Damien Rochford <djrochford@gmail.com>
2020-05-22 20:26:41 -0500

    Correction to exercise 4.48

--- Chapter 4/exercise_48.scm
+++ Chapter 4/exercise_48.scm
@@ -20,7 +20,7 @@ Here is the noun-phrase parsing procedures, from the book:"
 "We'll extend this by extending what counts as a simple noun phrase. In English, it is grammatical
 to put any number of adjectives (including zero) between the article and the noun in the noun-phrase.
 To accommodate this fact, we'll introduce two new syntactic categories: an adjective-string, which
-can be either an adjective or an adjective followed by an adjective-string, and an extended noun, which
+can be either an adjective or an adjective-string followed by an adjective, and an extended noun, which
 can be either a noun or an adjective-string and a noun. It looks like this:"
 
 (define (parse-adjective-string)
